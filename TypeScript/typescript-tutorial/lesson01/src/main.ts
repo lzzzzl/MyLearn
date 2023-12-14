0740f456d42c08a284031cdd4098d83e94b2230c
lzzzzl <lzzzzl005@gmail.com>
2023-12-14 22:44:39 +0800

    lesson 1 finish

--- TypeScript/typescript-tutorial/lesson01/src/main.ts
+++ TypeScript/typescript-tutorial/lesson01/src/main.ts
@@ -1,7 +1,10 @@
 let username = "lzzzzl";
 console.log(username);
 
-let a = 12;
-let b = '6';
-let c = 2;
+let a: number = 12;
+let b: number = 6;
+let c: number = 2;
 
+console.log(a / b);
+
+console.log(a * b);
